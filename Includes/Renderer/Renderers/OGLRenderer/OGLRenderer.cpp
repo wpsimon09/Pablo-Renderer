1a745174a0f09aa50598d2a2f5f89b38a04ca9d4
Šimon Potočňák <simon.potocnak03@gmail.com>
2024-02-05 19:12:40 +0100

    render buffer is displaying everything

--- Includes/Renderer/Renderers/OGLRenderer/OGLRenderer.cpp
+++ Includes/Renderer/Renderers/OGLRenderer/OGLRenderer.cpp
@@ -13,7 +13,7 @@ OGLRenderer::OGLRenderer(Scene *scene,  GLFWwindow* window) {
 void OGLRenderer::render(FrameBuffer* frameBuffer) {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glClearColor(0.11f, 0.11f, 0.11f, 1.0f);
-    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    frameBuffer->bind();
 
         this->scene->update();
 
