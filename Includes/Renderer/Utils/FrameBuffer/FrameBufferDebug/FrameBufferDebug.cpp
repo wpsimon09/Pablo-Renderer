ef8bb7d6b11378ab14f130a720f960436acee1cc
Šimon <95319163+wpsimon09@users.noreply.github.com>
2024-02-06 23:53:13 +0100

    Created correct geometry in debug fraˇe buffer
    This will intentionally trow an error as i am trying this on phone

--- Includes/Renderer/Utils/FrameBuffer/FrameBufferDebug/FrameBufferDebug.cpp
+++ Includes/Renderer/Utils/FrameBuffer/FrameBufferDebug/FrameBufferDebug.cpp
@@ -6,4 +6,5 @@
 
 FrameBufferDebug::FrameBufferDebug(int width, int height) : FrameBuffer(width, height) {
     this->shader = new Shader("VertexShader/FrameBufferDebugVertex.glsl" , "FragmentShader/FrameBufferDebugFragment.glsl", "FrameBufferDebug shader");
+    this->objectGeometry = new DebugFrameBufferGeometrz();
 }
