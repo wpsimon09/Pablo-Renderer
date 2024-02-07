b7f4968785f3ffa27653474d987d449c1f193004
Šimon Potočňák <simon.potocnak03@gmail.com>
2024-02-07 12:29:26 +0100

    the debug frame buffer is rendering

--- Includes/Renderer/Utils/FrameBuffer/FrameBufferDebug/FrameBufferDebug.cpp
+++ Includes/Renderer/Utils/FrameBuffer/FrameBufferDebug/FrameBufferDebug.cpp
@@ -5,6 +5,7 @@
 #include "FrameBufferDebug.h"
 
 FrameBufferDebug::FrameBufferDebug(int width, int height) : FrameBuffer(width, height) {
-    this->shader = new Shader("VertexShader/FrameBufferDebugVertex.glsl" , "FragmentShader/FrameBufferDebugFragment.glsl", "FrameBufferDebug shader");
+    this->shader = new Shader("VertexShader/FrameBufferDebugVertex.glsl" , "FragmentShader/Debug/FrameBufferDebug2.glsl", "FrameBufferDebug shader");
     this->objectGeometry = new FrameBufferDebugGeometry();
+    this->objectMaterial = new BasicMaterialTextured(this->shader, this->colorAttachment);
 }
