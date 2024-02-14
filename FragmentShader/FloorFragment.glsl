e0f8c06bb3babc03de062272c291a4be204bde7b
Šimon Potočňák <simon.potocnak03@gmail.com>
2024-02-14 12:49:07 +0100

    removed branching from grid

--- FragmentShader/FloorFragment.glsl
+++ FragmentShader/FloorFragment.glsl
@@ -9,18 +9,9 @@ in VS_OUT {
 
 out vec4 FragColor;
 
-
-uniform sampler2D texture_diffuse0;
-
 void main()
 {
-
     vec3 color = vec3(1.0);
-    if (abs(gl_FragCoord.x - gl_FragCoord.y) < 0.1 || abs(gl_FragCoord.x + gl_FragCoord.y - 800) < 0.1) {
-        color = vec3(1.0, 0.0,0.0);
-    }
-    if (abs(gl_FragCoord.x - gl_FragCoord.y - 400) < 0.1 || abs(gl_FragCoord.x + gl_FragCoord.y - 1200) < 0.1) {
-        color = vec3(0.0,0.0,1.0);
-    }
+
     FragColor = vec4(color, 0.9);
 }
