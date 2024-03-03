e3935207cc4da27cb756d6be6d64d514523e18fe
Šimon Potočňák <simon.potocnak03@gmail.com>
2024-03-03 16:37:36 +0100

    kept the GL_RG16F instead of the GL_RGBA16F

--- Includes/Renderer/Light/IBLPipeline/Stages/BRDF/BRDF.cpp
+++ Includes/Renderer/Light/IBLPipeline/Stages/BRDF/BRDF.cpp
@@ -9,7 +9,7 @@ BRDF::BRDF() {
 }
 
 void BRDF::execute() {
-    auto colorAttachemnt = std::make_unique<Texture2D>(512,512,GL_RGBA16F);
+    auto colorAttachemnt = std::make_unique<Texture2D>(512,512,GL_RG16F);
     colorAttachemnt->setUnpackAlignment(2);
     this->frameBuffer = std::make_unique<FrameBuffer>(colorAttachemnt->texWidth,colorAttachemnt->texHeight, this->shader,std::move(colorAttachemnt));
     this->frameBuffer->drawInsideSelf();
