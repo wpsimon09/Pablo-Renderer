8df24f8c58d0df8f5c517265b845b5992de28686
Šimon Potočňák <simon.potocnak03@gmail.com>
2024-03-15 20:44:57 +0100

    create sphere generator

--- Includes/Renderer/Geometry/Shapes/Custom/ModelGeometry.cpp
+++ Includes/Renderer/Geometry/Shapes/Custom/ModelGeometry.cpp
@@ -11,14 +11,15 @@ ModelGeometry::ModelGeometry(std::string name, std::vector<Vertex> verticies,std
     this->name = std::move(name);
     this->shapes = GL_TRIANGLES;
     this->vao = std::make_unique<VAO>(std::move(verticies), std::move(indecies));
+    this->numIndecies = this->vao->getNumberOfIndecies();
+    this->numVerticies = this->vao->getNumberOfVerticies();
 }
 
 void ModelGeometry::render() const {
     this->vao->bind();
-    glDrawElements(this->shapes, this->vao->getNumberOfIndecies(), GL_UNSIGNED_INT, 0);
+    glDrawElements(this->shapes, this->numIndecies, GL_UNSIGNED_INT, 0);
 
     this->vao->unbind();
-    glActiveTexture(GL_TEXTURE0);
     glGetError();
 }
 
