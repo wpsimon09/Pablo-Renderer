3b3408a9cb5f855fbe4866ec46a1f18c75f87cf4
Šimon Potočňák <simon.potocnak03@gmail.com>
2024-03-23 10:21:28 +0100

    added chromatic abriviation postrocessing effect

--- FragmentShader/FrameBufferDebugFragment.glsl
+++ FragmentShader/FrameBufferDebugFragment.glsl
@@ -6,9 +6,26 @@ in vec2 TexCoords;
 
 uniform sampler2D FragmentColor;
 
+
+float redOffset   =  0.009;
+float greenOffset =  0.006;
+float blueOffset  = -0.006;
+
+vec2 mouseFocusPoint = vec2(0.5,0.5);
+
 void main(){
     vec3 color = texture(FragmentColor, TexCoords).rgb;
+
+    vec2 texSize = textureSize(FragmentColor, 0).xy;
+    vec2 texCoord = gl_FragCoord.xy / texSize;
+
+    vec2 direction =  texCoord- mouseFocusPoint;
+
     //color = pow(color,vec3(1.0/1.2));
-    FragColor = vec4(color,1.0);
+    //FragColor = vec4(color,1.0);
+
+    FragColor.r  = texture(FragmentColor, texCoord + (direction * vec2(redOffset  ))).r;
+    FragColor.g  = texture(FragmentColor, texCoord + (direction * vec2(greenOffset))).g;
+    FragColor.ba = texture(FragmentColor, texCoord + (direction * vec2(blueOffset ))).ba;
 
 }
