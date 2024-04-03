aeac2628d00c34d76454bd7397fe4ccf1e907aea
Šimon Potočňák <simon.potocnak03@gmail.com>
2024-04-03 07:18:57 +0200

    altered width and height of the shadow map

--- Includes/Renderer/RenderPass/ShadowMapPass/ShadowMapPass.cpp
+++ Includes/Renderer/RenderPass/ShadowMapPass/ShadowMapPass.cpp
@@ -6,7 +6,7 @@
 
 
 ShadowMapPass::ShadowMapPass() {
-    this->frameBuffer = std::make_unique<FrameBuffer>(1200, 1200);
+    this->frameBuffer = std::make_unique<FrameBuffer>(800, 600);
 }
 
 std::shared_ptr<Texture2D> ShadowMapPass::render(std::shared_ptr<Scene> scene, std::shared_ptr<OGLRenderer> renderer) {
