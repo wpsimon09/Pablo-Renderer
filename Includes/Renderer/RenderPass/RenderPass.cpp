fef438009ff65490175e66a1e2ee771316e36820
Šimon Potočňák <simon.potocnak03@gmail.com>
2024-04-14 14:30:25 +0200

    predelite unnecesariy files push

--- Includes/Renderer/RenderPass/RenderPass.cpp
+++ Includes/Renderer/RenderPass/RenderPass.cpp
@@ -4,7 +4,6 @@
 
 #include "RenderPass.h"
 #include "Renderer/Utils/GLFWHelper/GLFWHelper.h"
-#include "imgui/imgui.h"
 
 RenderPass::RenderPass() {
     this->renderPassResult = std::make_shared<Texture2D>(GLFWHelper::getScreenWidth(), GLFWHelper::getScreenHeight(), GL_RGBA16F);
