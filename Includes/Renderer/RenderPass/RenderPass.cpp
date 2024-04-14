b46da71605ecd5365cb13db90314dad29970c1ea
Šimon Potočňák <simon.potocnak03@gmail.com>
2024-04-14 13:22:42 +0200

    improved quality of the cmakefile

--- Includes/Renderer/RenderPass/RenderPass.cpp
+++ Includes/Renderer/RenderPass/RenderPass.cpp
@@ -4,6 +4,7 @@
 
 #include "RenderPass.h"
 #include "Renderer/Utils/GLFWHelper/GLFWHelper.h"
+#include "imgui/imgui.h"
 
 RenderPass::RenderPass() {
     this->renderPassResult = std::make_shared<Texture2D>(GLFWHelper::getScreenWidth(), GLFWHelper::getScreenHeight(), GL_RGBA16F);
@@ -11,6 +12,7 @@ RenderPass::RenderPass() {
 
 void RenderPass::addInput(std::shared_ptr<TextureBase> input) {
     this->inputs.push_back(input);
+
 }
 
 void RenderPass::prepareForNextFrame() {
