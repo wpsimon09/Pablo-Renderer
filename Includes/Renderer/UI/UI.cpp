7e0937a72bd152953fe383cf6a7c47eb63530361
Šimon Potočňák <simon.potocnak03@gmail.com>
2024-04-15 22:30:46 +0200

    added statistics UI

--- Includes/Renderer/UI/UI.cpp
+++ Includes/Renderer/UI/UI.cpp
@@ -12,8 +12,22 @@ void UI::render() {
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
 
-    ImGui::ShowDemoWindow(&shadow_demo_window);
+    static float f = 0.0f;
+    static int counter = 0;
 
+    ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+    ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+
+    ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+
+    if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+        counter++;
+    ImGui::SameLine();
+    ImGui::Text("counter = %d", counter);
+
+    ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / imGuiIo->Framerate, imGuiIo->Framerate);
+    ImGui::End();
     ImGui::Render();
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
