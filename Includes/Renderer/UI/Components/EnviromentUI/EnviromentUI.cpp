71855ee339b185ed100e4b484aaa496ab30cbc67
Šimon Potočňák <simon.potocnak03@gmail.com>
2024-05-24 20:47:32 +0200

    hdri texture is not rendering because imgui can not read it

--- Includes/Renderer/UI/Components/EnviromentUI/EnviromentUI.cpp
+++ Includes/Renderer/UI/Components/EnviromentUI/EnviromentUI.cpp
@@ -8,13 +8,34 @@
 #include "Renderer/PabloRenderer.h"
 
 void EnviromentUI::display() {
+    auto instance = PabloRenderer::getInstance();
+    auto iblPiplineInstance = instance->getScene()->getIblPipeLine();
+
     ImGui::Begin("Environment settings");
 
     ImGui::SetWindowSize(ImVec2(400, 400));
 
-    auto instance = PabloRenderer::getInstance();
-    FileWindowUI::supportedFiles = ".hdr";
+    ImGui::Text("Environment preview");
+
+    ImVec2 imageSize((float) iblPiplineInstance->iblTextures[0]->type->texWidth / 4, (float) iblPiplineInstance->iblTextures[0]->type->texHeight / 4);
+        ImGui::BeginChild("##", imageSize);
+        ImGui::GetWindowDrawList()->AddImage(
+                (void *) iblPiplineInstance->iblTextures[0]->type->ID,
+                ImGui::GetCursorScreenPos(), // Use cursor screen position as top-left corner
+                ImVec2(ImGui::GetCursorScreenPos().x + imageSize.x ,
+                       ImGui::GetCursorScreenPos().y + imageSize.y ), // Use bottom-right corner
+                ImVec2(0, 1),
+                ImVec2(1, 0)
+        );
+        ImGui::EndChild();
+
+    //--------------------------
+    // LOADING NEW IBL PIPELINE
+    //--------------------------
+    EnviromentUI::hdrTexturePath = FileWindowUI::supportedFiles = ".hdr";
     FileWindowUI::display();
+
+
 
     if(ImGui::Button("Close")){
         SceneMenu::showEnviromentMenu = false;
--- Includes/Renderer/UI/Components/EnviromentUI/EnviromentUI.h
+++ Includes/Renderer/UI/Components/EnviromentUI/EnviromentUI.h
@@ -5,9 +5,12 @@
 #ifndef PABLO_RENDERER_ENVIROMENTUI_H
 #define PABLO_RENDERER_ENVIROMENTUI_H
 
+#include "iostream"
 #include "Renderer/UI/Components/Component.h"
 
 class EnviromentUI:public Component {
+private:
+    static inline std::string hdrTexturePath ="";
 public:
     static void display();
 };
